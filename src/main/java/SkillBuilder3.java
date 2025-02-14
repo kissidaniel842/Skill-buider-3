5c9124c9c9fe54434b33e6b4f6a2761c8d346ee7
Daniel Kissi <kissidaniel842@gmail.com>
2025-02-14 13:08:00 -0500

    Done!

--- src/main/java/SkillBuilder3.java
+++ src/main/java/SkillBuilder3.java
@@ -2,7 +2,7 @@
  * SkillBuilder3 is a class for completing
  *  Skill Builder 3 in Java.
  *
- * @author <You>
+ * @Daniel Kissi <You>
  * @version 1.0
  */
 
@@ -11,43 +11,76 @@ public class SkillBuilder3
     private static String repeat(String s, int numOfTimesToRepeat)
     {
         String result = "";
-        // TODO: replace this line with your code
+        for (int i = 0; i < numOfTimesToRepeat; i++) {
+            result += s;
+        }
         return result;
     }
     public static String leftRightTriangle(int height)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = 1; i <= height; i++) {
+            str += repeat("*", i) + "\n";
+        }
         return str;
     }
 
     public static String rightRightTriangle(int height)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = 1; i <= height; i++) {
+            str += repeat(" ", height - i) + repeat("*", i) + "\n";
+        }
         return str;
     }
 
     public static String circle(int radius)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int y = radius - 1; y >= -radius + 1; y--) {
+            int x = (int) Math.sqrt(radius * radius - y * y);
+            str += repeat(" ", radius - x) + repeat("*", 2 * x) + "\n";
+        }
         return str;
     }
 
     public static long sumOfDivisors(long number) {
         long sum = 0;
-        // TODO: replace this line with your code
+        for (long i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
         return sum;
     }
 
     public static boolean isPerfect(long number){
-        return false;
+        return sumOfDivisors(number) == number;
     }
+
+    public static boolean isPrime(long number) {
+        if (number < 2) return false;
+        if (number == 2 || number == 3) return true;
+        if (number % 2 == 0 || number % 3 == 0) return false;
+        for (long i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
     public static long sumOfDivisorsUsingWhile(long number){
         long sum = 0;
-        // TODO: replace this line with your code
+        long i = 1;
+        while (i <= number / 2) {
+            if (number % i == 0) {
+                sum += i;
+            }
+            i++;
+        }
         return sum;
     }
 }
